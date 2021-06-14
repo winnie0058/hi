8ec898611a5673918accd903dd32e6161e5c09a0
Winnie Tsao <winnie0058@gmail.com>
2021-06-14 14:38:29 +0800

    for loop

---
+++ list.py
@@ -0,0 +1,22 @@
+# a = ['Toyota', 'Honda', 'Audi']
+# print(a)
+# a.append('Susuki')
+# print(a)
+# print(len(a))
+
+# print('Audi' in a)
+# print('Benz' in a)
+
+cars = ['Toyota', 'Honda']
+
+for car in cars:
+	print('car = ', car)
+
+# 字串當清單
+car = 'Audi'
+for c in car:
+	print(c)
+
+names = ['Winnie', 'Betty', 'Jenny']
+for name in names:
+	print('name = ', name)
